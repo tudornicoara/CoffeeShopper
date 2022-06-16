4d8925ba7dde121ce573db5e6fe57c6d40ea5067
Tudor Nicoara <nicoaratudor@gmail.com>
2022-06-16 14:44:09 +0300

    Set up Identity server and moved database to DataAccess project

---
+++ Server/Program.cs
@@ -0,0 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var assembly = typeof(Program).Assembly.GetName().Name;
+var defaultConnString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddIdentityServer()
+    .AddConfigurationStore(options =>
+    {
+        options.ConfigureDbContext = b =>
+            b.UseSqlite(defaultConnString, opt => opt.MigrationsAssembly(assembly));
+    }).AddOperationalStore(options =>
+    {
+        options.ConfigureDbContext = b =>
+            b.UseSqlite(defaultConnString, opt => opt.MigrationsAssembly(assembly));
+    })
+    .AddDeveloperSigningCredential();
+
+var app = builder.Build();
+
+app.UseIdentityServer();
+
+app.Run();
