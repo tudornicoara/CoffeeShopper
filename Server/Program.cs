0d343c1049697ad5a6bd7b28047d1f7f2b635944
Tudor Nicoara <nicoaratudor@gmail.com>
2022-06-16 15:59:41 +0300

    Setup Server with Quickstart.UI

--- Server/Program.cs
+++ Server/Program.cs
@@ -40,6 +40,16 @@ builder.Services.AddIdentityServer()
 
 var app = builder.Build();
 
+app.UseStaticFiles();
+app.UseRouting();
+
 app.UseIdentityServer();
+
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapDefaultControllerRoute();
+});
 
 app.Run();
