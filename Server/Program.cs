f1a86663ec3ad1577fb20dd26da184733eef409b
Tudor Nicoara <nicoaratudor@gmail.com>
2022-06-16 15:34:25 +0300

    Set up IdentityServer Config and SeedData

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,11 +1,32 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Server;
+using Server.Data;
+
+var seed = args.Contains("/seed");
+if (seed)
+{
+    args = args.Except(new[] {"/seed"}).ToArray();
+}
 
 var builder = WebApplication.CreateBuilder(args);
 
 var assembly = typeof(Program).Assembly.GetName().Name;
 var defaultConnString = builder.Configuration.GetConnectionString("DefaultConnection");
 
+if (seed)
+{
+    SeedData.EnsureSeedData(defaultConnString);
+}
+
+builder.Services.AddDbContext<AspNetIdentityDbContext>(options =>
+    options.UseSqlite(defaultConnString, b => b.MigrationsAssembly(assembly)));
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+    .AddEntityFrameworkStores<AspNetIdentityDbContext>();
+
 builder.Services.AddIdentityServer()
+    .AddAspNetIdentity<IdentityUser>()
     .AddConfigurationStore(options =>
     {
         options.ConfigureDbContext = b =>
