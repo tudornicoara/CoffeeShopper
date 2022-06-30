94987adabddc31c6642132d2565dcccd77a48646
Tudor Nicoara <nicoaratudor@gmail.com>
2022-06-30 10:21:58 +0300

    Login, Logout page

---
+++ Client/Pages/Login.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Client.Pages;
+
+public class LoginModel : PageModel
+{
+    public async Task<IActionResult> OngGetAsync(string redirectUri)
+    {
+        if (string.IsNullOrEmpty(redirectUri))
+        {
+            redirectUri = Url.Content("~/");
+        }
+
+        if (HttpContext.User.Identity.IsAuthenticated)
+        {
+            Response.Redirect(redirectUri);
+        }
+
+        return Challenge(
+            new AuthenticationProperties
+            {
+                RedirectUri = redirectUri
+            },
+            OpenIdConnectDefaults.AuthenticationScheme);
+    }
+}
---
+++ Client/Shared/RedirectToLogin.razor.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Client.Shared;
+
+public partial class RedirectToLogin
+{
+    [Inject] private NavigationManager Navigation { get; set; }
+
+    protected override async Task OnInitializedAsync()
+    {
+        Navigation.NavigateTo($"/login?redirectUri={Uri.EscapeDataString(Navigation.Uri)}", true);
+    }
+}
+
